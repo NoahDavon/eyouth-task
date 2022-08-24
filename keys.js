189551be266fc86ec50fd35b0e01b81ef9b07b6b
noahdude <mohamadfahmy@thebadnomads.com>
2022-08-24 21:03:14 +0200

    Requests

---
+++ keys.js
@@ -0,0 +1,1 @@
+export const keys =  {"tmdb": "59ac30d7a25d364bedaee9f99cd0fb37"}
--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,25 @@ export default function Home() {
         <title>EBooth - Your Next Best Movie</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div class="flex flex-col text-white mx-auto my-20 w-fit text-center">
-        <div class="font-extrabold text-6xl">
+      <div className="flex flex-col text-white mx-auto my-20 w-fit text-center">
+        <div className="font-extrabold text-6xl">
           Who's Ready For Movie Night?      
         </div>
-        <Link href="#" passHref>
-        <a class="bg-blue-700 rounded-xl w-fit px-6 py-2 mx-auto mt-6 hover:bg-blue-500">Playing Now</a>
-        </Link>
-        <Link href="#" passHref>
-        <a class="bg-blue-700 rounded-xl w-fit px-6 py-2 mx-auto mt-6 hover:bg-blue-500">Most Popular</a>
-        </Link>
+        <div className='flex flex-col sm:flex-row'>
+          <MainButton href="/movies/?src=playing">Now Playing</MainButton>
+          <MainButton href="/movies/?src=popular">Most Popular</MainButton>
+          <MainButton href="/movies/?src=latest">Latest</MainButton>
+          <MainButton href="/movies/?src=top">Top Rated</MainButton>
+          <MainButton href="/movies/?src=upcoming">Upcoming</MainButton>
+        </div>
         
       </div>
     </div>
   )
 }
+function MainButton({href, children}) {
+  return <Link href={href} passHref>
+    <a className="bg-blue-700 rounded-xl w-fit px-6 py-2 mx-auto mt-6 hover:bg-blue-500">{children}</a>
+  </Link>
+}
+
